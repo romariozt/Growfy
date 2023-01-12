841fc58d6873a2bafb28997b02b85d91c6d1401b
Roman <108659195+romariozt@users.noreply.github.com>
2023-01-12 17:29:36 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,21 +8,3 @@ function documentClick(e) {
     document.documentElement.classList.toggle('menu-open');
   }
 }
-
-
-
-
-
-// ?Кнопка на верх
-// -------------------------------------//
-$(window).on('scroll', function () {
-  if ($(this).scrollTop() > 700){
-    $('#up').fadeIn()
-  }else{
-    $('#up').fadeOut()
-  }
-})
-
-$('#up').on('click', function () {
-  $('html, body').animate({scrollTop: 0}, 500)
-})
